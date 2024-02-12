831bec48ddbddd88e6d2be05c6feb75481e95043
Victor Ifah <97197194+GerromeSieger@users.noreply.github.com>
2024-02-12 10:43:43 +0100

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,1 +1,4 @@
+
+minikube addons enable ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml
+echo "$(minikube ip) app.victor.com" | sudo tee -a /etc/hosts
